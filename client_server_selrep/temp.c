4b99b91ac50f949c9540906f2ec4e3ce4c6136d6
Nikti <contact.nikti@gmail.com>
2022-05-23 21:23:44 +0530

    ARQ added

--- client_server_selrep/temp.c
+++
@@ -1,6 +0,0 @@
-printf("\n[-]Timeout!!!\n");
-                        printf("\n[-]NAK %d\n",k);
-                        printf("\n[-]Frame  %d sent again\n",k);
-                        sprintf(buff1,"%d",k);
-                        buff[0] = buff1[0];
-                        write(c_fd,buff,100);
